1a3470ce8768fc002c387bc5c92d9321abe3a1a8
Jose Manuel <jecheverri@vozy.co>
2022-01-07 16:48:46 -0500

    Avance en logica para crear arhivo de resultados

--- tests/agent_chat_test.py
+++ tests/agent_chat_test.py
@@ -8,6 +8,7 @@ from assertpy import assert_that
 import pandas as pd
 from datetime import datetime
 import time
+import csv
 
 agent = "e140ccbd-acb2-4e80-b3ae-f098372ac4d9"
 
@@ -15,54 +16,68 @@ url = " https://api.vozy.ai/v2/chat/"+agent
 
 
 headers = {
-    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhYdGVwY3ZLQjVBQzQxQ1JnbzZjViJ9.eyJpc3MiOiJodHRwczovL2xvZ2luLnZvenkuYWkvIiwic3ViIjoiYXV0aDB8NjE5NTUzM2FjMGQyODQwMDcwNWY5MWIwIiwiYXVkIjpbImh0dHBzOi8vYXBpLnZvenkuYWkvYXV0aCIsImh0dHBzOi8vdm96eS51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjQxMjIzNTY3LCJleHAiOjE2NDEzMDk5NjcsImF6cCI6IjhzRWNIbkpHaERmc2k2QnpXTDdqNHRQazlmNjJQdUlvIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsIm9yZ19pZCI6Im9yZ19nUVA4aGRJYU4xeTR0RTI2In0.l66OYfK_RP_MHNQCd7buC2Evo31XGX15RRNpEDFvetIr3AYVOlD144aDDyj-adcvBY8vsbajee-cEd2iuOfhbqf8h_1IRNNREC0ZT-CK-fKwKgESCSFAfECjscR6dEU45IB0wB992Ei0lzl0bPgjS-g5l85s6dnwXCrDx-4VIqa_-eNaBwutcxWLKnE7VRrH6oFdGvxUnoFf8WsdsbCBjfzfVtLydk-OTi9Z_qMFgdKxl1lP1H-qhTJcNPNeXIVuTi2EcLWa0npvvgk3JuijhFVc7rq1CEI-Ehhc9AdXhtxIlWL4E_wm6TRmKGW6SgOgxN9Jy5g2jGeVar6HRpm5mg',
+    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhYdGVwY3ZLQjVBQzQxQ1JnbzZjViJ9.eyJpc3MiOiJodHRwczovL2xvZ2luLnZvenkuYWkvIiwic3ViIjoiYXV0aDB8NjE5NTUzM2FjMGQyODQwMDcwNWY5MWIwIiwiYXVkIjpbImh0dHBzOi8vYXBpLnZvenkuYWkvYXV0aCIsImh0dHBzOi8vdm96eS51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjQxNTgzOTE0LCJleHAiOjE2NDE2NzAzMTQsImF6cCI6IjhzRWNIbkpHaERmc2k2QnpXTDdqNHRQazlmNjJQdUlvIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsIm9yZ19pZCI6Im9yZ19nUVA4aGRJYU4xeTR0RTI2In0.PWPJQLqeOieuH57_zQb3tEE4I2M34v8sqzl5XQ5HffsAjf0rKoiyj8v0xWHt2cXOjbZqkp4fLYa4Wbqxui49Uz_4vrPMYTkRzCY4UMQeguUZqhULZinKaE9oLZpYCQuClzvHvdulwbfgThz0bn0UgfA9Hn3TDhu0EdDk-fb5rVAmgrQ_IOVj1xnhxBs6YzM_MiUjDfSZ7niS7_pjCWwI1OJftbN_7p8mU1_Qpa6gdbufBPYIT_PaNBJy5PhpT9SqMWoU2-ODMtlvdV6n0NIp4Vr35-n5pawV98vrG8BcnqCzOXipse8rLRXQiSrKSub1N4gc3wCitWNj7Ij3d3v1LQ',
     'Content-Type': 'application/json'
 }
 
 
 class Chats:
-    def __init__(self, dict_chats, dict_lili_responses):
+    def __init__(self, dict_chats, list_lili_responses, list_intents_names):
         self.dict_chats = dict_chats
-        self.dict_lili_responses = dict_lili_responses
+        self.list_lili_responses = list_lili_responses
+        self.list_intents_names = list_intents_names
 
     def get_dict_chats(self):
         return self.dict_chats
 
-    def get_dict_responses(self):
-        return self.dict_lili_responses
+    def get_list_responses(self):
+        return self.list_lili_responses
+
+    def get_list_intents_names(self):
+        return self.list_intents_names
 
     def __str__(self):
-        return str(self.dict_chats)+str(self.dict_lili_responses)
+        return str(self.dict_chats)+'--'+str(self.list_lili_responses)+'--'+str(self.list_intents_names)
 
 
 def test_chat_with_lili():
 
-    dic = load_file_with_data('debt_collection_flow1.csv')
+    test_file_name = 'debt_collection_flow1.csv'
+
+    dic = load_file_with_data(test_file_name)
 
     chats_to_test = get_chats_to_test(dic)
 
-    for i, list_of_posibble_responses in chats_to_test.get_dict_chats().items():
+    with open('results_'+test_file_name, 'w', encoding='UTF8') as f:
 
-        now = datetime.now()
-        identificator = now.strftime("%H:%M:%S")
-        init_chat_with_lili(identificator)
+        writer = csv.writer(f)
+        header = chats_to_test.get_list_intents_names()
+        writer.writerow(header)
 
-        for j, utterancce in enumerate(list_of_posibble_responses):
-            payload = json.dumps({
-                "from": identificator,
-                "body": utterancce})
+        for i, list_of_posibble_responses in chats_to_test.get_dict_chats().items():
 
-            time.sleep(1.5)
+            now = datetime.now()
+            identificator = now.strftime("%H:%M:%S")
+            init_chat_with_lili(identificator)
 
-            response = requests.request(
-                "POST", url, headers=headers, data=payload)
+            for j, utterancce in enumerate(list_of_posibble_responses):
 
-            print('Humano :', utterancce)
-            print('Lili :', response.text)
+                payload = json.dumps({
+                    "from": identificator,
+                    "body": utterancce})
+                time.sleep(1.5)
 
-            assert_that(response.status_code).is_equal_to(requests.codes.ok)
-            assert_that(response.json()['message']).is_not_empty()
-            # assert_that(response.json()['message']).is_equal_to_ignoring_case(chats_to_test.get_dict_responses()[index])
+                # response = requests.request(
+                #     "POST", url, headers=headers, data=payload)
+
+                # print('Humano :', utterancce)
+                # print('Lili :', response.text)
+                row = list_of_posibble_responses
+                writer.writerow(row)
+                # assert_that(response.status_code).is_equal_to(
+                #     requests.codes.ok)
+                # assert_that(response.json()['message']).is_not_empty()
+                # assert_that(response.json()['message']).is_equal_to_ignoring_case(chats_to_test.get_list_responses()[j])
 
 
 def init_chat_with_lili(identificator):
@@ -80,22 +95,28 @@ def load_file_with_data(file_name):
 
 
 def get_chats_to_test(chats_to_test):
+
     list_of_node_names = list(chats_to_test.keys())
     first_node = list_of_node_names[0]
     len_of_node = len(chats_to_test[first_node])-1
     dict_with_conversation_flows = {}
-    dict_with_node_names_responses = []
+    list_with_node_names_responses = []
 
     for x in range(len_of_node):
         dict_with_conversation_flows[x] = []
 
     for i, node in enumerate(list_of_node_names):
-        dict_with_node_names_responses.append(chats_to_test[node].pop())
+
+        list_with_node_names_responses.append(chats_to_test[node].pop())
+
         for j, user_text in enumerate(chats_to_test[node]):
+
             list_to_add = dict_with_conversation_flows[j]
             list_to_add.append(user_text)
             dict_with_conversation_flows[j] = list_to_add
-    chats = Chats(dict_with_conversation_flows, dict_with_node_names_responses)
+
+    chats = Chats(dict_with_conversation_flows,
+                  list_with_node_names_responses, list_of_node_names)
     return chats
 
 
