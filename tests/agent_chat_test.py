982c52dda0d9bbb6a1f9fdbc864e4fe2932c2516
Jose Manuel <jecheverri@vozy.co>
2022-01-27 11:47:49 -0500

    Stable version

--- tests/agent_chat_test.py
+++ tests/agent_chat_test.py
@@ -10,13 +10,9 @@ from datetime import datetime
 import time
 import csv
 
-agent = "e140ccbd-acb2-4e80-b3ae-f098372ac4d9"
-
-url = " https://api.vozy.ai/v2/chat/"+agent
-
 
 headers = {
-    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhYdGVwY3ZLQjVBQzQxQ1JnbzZjViJ9.eyJpc3MiOiJodHRwczovL2xvZ2luLnZvenkuYWkvIiwic3ViIjoiYXV0aDB8NjE5NTUzM2FjMGQyODQwMDcwNWY5MWIwIiwiYXVkIjpbImh0dHBzOi8vYXBpLnZvenkuYWkvYXV0aCIsImh0dHBzOi8vdm96eS51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjQzMTI4NjA2LCJleHAiOjE2NDMyMTUwMDYsImF6cCI6IjhzRWNIbkpHaERmc2k2QnpXTDdqNHRQazlmNjJQdUlvIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsIm9yZ19pZCI6Im9yZ19nUVA4aGRJYU4xeTR0RTI2In0.nihRZ_lC9j-eKiv4Y_AH5AX3dHqSz5rg6nSpJFybyRuX6GhNmIe8Et9RO4gLVztMzkiS7TNdXGl12BPGgTpPqLBp_y_i1MO9z5vm3W6AEE7QWB316EOD09ClExHil-Gxc8Sg8XSGkx7Br-Qj380R5sY_B6If1zmgJyzLhTGLUBDd9Rx9TtvKqia8JIMmxidvbHt2QcnnGRgF-DzMuSq9CKGNY8k6x2WUWCFEyeXU950uk8qo4GzW_dUZ7DWU_wKJGggDmtUdWXzfJorQSGXstWjmytShi_vU-SJBDwnVR4G9kYHgIV9Wadcak_0sqaS3Iwbj5kLYmtBeJvsyVAmbew',
+    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhYdGVwY3ZLQjVBQzQxQ1JnbzZjViJ9.eyJpc3MiOiJodHRwczovL2xvZ2luLnZvenkuYWkvIiwic3ViIjoiYXV0aDB8NjE5NTUzM2FjMGQyODQwMDcwNWY5MWIwIiwiYXVkIjpbImh0dHBzOi8vYXBpLnZvenkuYWkvYXV0aCIsImh0dHBzOi8vdm96eS51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjQzMjkzNjgyLCJleHAiOjE2NDMzODAwODIsImF6cCI6IjhzRWNIbkpHaERmc2k2QnpXTDdqNHRQazlmNjJQdUlvIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsIm9yZ19pZCI6Im9yZ19nUVA4aGRJYU4xeTR0RTI2In0.IiQUCKcp8vprtl7bbnZKSdCI4_4sE63DkVh4k5MrpIfSCrKkkGUaD1wWy-1iMfHqLxozPlcNQ3QMQV3kclPr-UQ4_NA9MdDvlckeRCxwPbN_c57Pfewe0rhCh3_-T5KwtTV_bxXgbHBWoqpNc69GjG6pGztB6gC3-4JDzzL45Xv4Gu7xYgZVpJbMiaVugUumn3EQymU55Z2nSQEi_91PeoxyqnItYavxDeuUUF3MWilpCqQbG8eS89jgG1H9Z81pB092D8DCONTAoABW_1NgTkiOZOaDau2jhnVHJO31NpWY9YzzXuRsvC5yK_bI-lSMp5q3Rv4AB3qAzAp2TQYd3A',
     'Content-Type': 'application/json'
 }
 
@@ -40,49 +36,55 @@ class Chats:
         return str(self.dict_chats)+'--'+str(self.list_lili_responses)+'--'+str(self.list_intents_names)
 
 
-def test_recall():
+def test_already_paid():
 
-    test_file_name = 'debt_collection_flow2.csv'
+    agent = "376147d1-d2f9-42aa-9be1-c280cf3f3ca6"
+
+    url = " https://api.vozy.ai/v2/chat/"+agent
+
+    test_file_name = 'QA_IPESOPE_final_testeo/gp_contact_no.csv'
 
     dic = load_file_with_data(test_file_name)
 
     chats_to_test = get_chats_to_test(dic)
 
-    with open('results_'+test_file_name, 'w', encoding='UTF8') as f:
+    for i, list_of_posibble_responses in chats_to_test.get_dict_chats().items():
 
-        writer = csv.writer(f)
-        header = chats_to_test.get_list_intents_names()
-        writer.writerow(header)
+        now = datetime.now()
+        identificator = now.strftime("%H:%M:%S")
+        init_chat_with_lili(identificator, url)
+        print('Flujo-'+str(i))
+        print('')
 
-        for i, list_of_posibble_responses in chats_to_test.get_dict_chats().items():
+        for j, utterancce in enumerate(list_of_posibble_responses):
 
-            now = datetime.now()
-            identificator = now.strftime("%H:%M:%S")
-            init_chat_with_lili(identificator)
-            print(list_of_posibble_responses)
+            payload = json.dumps({
+                "from": identificator,
+                "body": utterancce})
 
-            for j, utterancce in enumerate(list_of_posibble_responses):
+            time.sleep(1.6)
 
-                payload = json.dumps({
-                    "from": identificator,
-                    "body": utterancce})
-                time.sleep(1.6)
+            response = requests.request(
+                "POST", url, headers=headers, data=payload)
 
-                response = requests.request(
-                    "POST", url, headers=headers, data=payload)
+            print('Human message: '+utterancce)
+            print('Lili response: '+str(response.json()['message']))
+            print('')
 
-                assert_that(response.status_code).is_equal_to(
-                    requests.codes.ok)
-                assert_that(response.json()['message']).is_not_empty()
-                assert_that(response.json()['message'].strip()).is_equal_to_ignoring_case(
-                    chats_to_test.get_list_responses()[j].strip())
+            assert_that(response.status_code).is_equal_to(
+                requests.codes.ok)
+            assert_that(response.json()['message']).is_not_empty()
+            # assert_that(response.json()['message'].strip()).is_equal_to_ignoring_case(
+            #     chats_to_test.get_list_responses()[j].strip())
 
 
-def init_chat_with_lili(identificator):
+def init_chat_with_lili(identificator, url):
+
     payload = json.dumps({
         "from": identificator,
         "body": "Iniciar conversacion"})
-    response = requests.request(
+
+    requests.request(
         "POST", url, headers=headers, data=payload)
 
 
@@ -118,3 +120,4 @@ def get_chats_to_test(chats_to_test):
     return chats
 
 
+test_already_paid()
